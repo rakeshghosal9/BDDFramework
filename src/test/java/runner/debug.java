03bec5c318e76b6a2f425b638fce2948bb6e1218
Rakesh Ghosal <rakeshghosal.nsec@gmail.com>
2023-01-06 16:34:43 +0530

    Added new test case to analysis stock

--- src/test/java/runner/debug.java
+++ src/test/java/runner/debug.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         glue = "stepdefinition",
         features = {"src\\test\\resources\\features\\stock_Analysis\\StockAnalysis.feature"}
-        //plugin = {"json:target/cucumber-report/Runner_LoginValidation_scenario001_run001_IT.json"}
+        plugin = {"json:target/cucumber-report/StockAnalysis.json"}
 )
 
 public class debug {
